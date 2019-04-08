187b04ba6b7e0721b2e1124650eaf99b99f2ff93
shubham <shubham.agarwal@hashedin.com>
2019-04-08 11:54:02 +0530

    Added addition and deletion of items in todolist

--- my-app/src/ToDoList/TodoList.js
+++ my-app/src/ToDoList/TodoList.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import TodoItems from '../TodoItems/TodoItems';
+import './TodoList.css';
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             items: []
-          };
+        };
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
     }
 
     addItem(e) {
@@ -41,7 +53,8 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
-                <TodoItems entries={this.state.items}/>
+                <TodoItems entries={this.state.items}
+                    delete={this.deleteItem} />
             </div>
         );
     }
